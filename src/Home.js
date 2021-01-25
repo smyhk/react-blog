10690ab60a69c72fa4f7f88e401eb96d46520bd4
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-01-24 16:06:58 -0800

    Pass functions as props

--- src/Home.js
+++ src/Home.js
@@ -13,9 +13,14 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 };
