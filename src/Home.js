976863eef5bce0b1da71b8d0a62596ece027964a
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-01-24 16:22:15 -0800

    Playing with useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,14 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState('mario');
+
+  // Runs with every render
+  useEffect(() => {
+    console.log('useEffect ran!');
+    console.log(name);
+  }, [name]);
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -21,6 +29,8 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
