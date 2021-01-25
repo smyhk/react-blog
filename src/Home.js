2761faba8ef1efebd8bd3a7270b64a3b9f29692b
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-01-25 10:05:02 -0800

    Handle errors

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,32 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState('');
 
   // Runs with every render
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if (!res.ok) {
+          throw Error('could not fetch data');
+        }
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home">
       {/* conditional templating */}
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
