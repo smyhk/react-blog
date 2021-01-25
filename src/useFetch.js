76dfaf943e11d9933b3d270e8bacd4bb29fafdc9
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-01-25 11:17:18 -0800

    Use an abort controller

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,7 +8,9 @@ const useFetch = (url) => {
 
   // Runs with every render
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error('could not fetch data');
@@ -21,9 +23,15 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted');
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       });
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
