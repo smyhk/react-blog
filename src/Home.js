694d52ca4cf1ba51894c4ed0ce4271159c56ade1
Steve Kedzie <seal.team.smyhk@gmail.com>
2021-01-25 09:46:02 -0800

    Fetch blog data from json-server

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,21 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState([]);
 
   const [name, setName] = useState('mario');
 
   // Runs with every render
   useEffect(() => {
-    console.log('useEffect ran!');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -28,9 +25,10 @@ const Home = () => {
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
-      <p>{name}</p>
+      {/* conditional templating */}
+      {blogs && (
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
